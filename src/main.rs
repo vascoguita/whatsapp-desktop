daa74cc589d3a3412c186e24ebd9e8b1d35e5e4c
Vasco Guita <vasco@guita.org>
2026-08-25 23:36:21 +0200

    Fix WebKitGTK Wayland crash and add missing GStreamer dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    let nvidia = std::path::Path::new("/sys/module/nvidia").exists();
+    let wayland = std::env::var("WAYLAND_DISPLAY").is_ok();
+
+    if nvidia && wayland {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
     app_lib::run();
 }
