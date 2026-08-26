182db5ec838501affc2e84411813278128ec88f8
Vasco Guita <vasco@guita.org>
2026-08-26 17:00:22 +0200

    fix: apply nvidia dmabuf renderer quirk to x11 as well

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    let nvidia = std::path::Path::new("/sys/module/nvidia").exists();
-    let wayland = std::env::var("WAYLAND_DISPLAY").is_ok();
-
-    if nvidia && wayland {
+    if std::path::Path::new("/sys/module/nvidia").exists() {
         std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
     }
     app_lib::run();
